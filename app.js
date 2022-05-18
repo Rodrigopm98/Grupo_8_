36b042e120c6ad8c79bf1a4cc4bd3b8d2d9c52fc
andresp90 <andresgpaulon@gmail.com>
2022-05-18 12:12:33 -0300

    agregue productCart y rutas en index

--- app.js
+++ app.js
@@ -9,3 +9,6 @@ app.use(express.static(publicpath));
 /* creacion del puerto 3030 */
 app.listen(3030, ()=>console.log("puerto creado en: http://localhost:3030/"));
 app.get("/", function(req,res){res.sendFile(path.resolve(__dirname, "./views/index.html"))});
+app.get("/productCart", function(req,res){res.sendFile(path.resolve(__dirname, "./views/productCart.html"))});
+app.get("/login", function(req,res){res.sendFile(path.resolve(__dirname, "./views/login.html"))});
+app.get("/register", function(req,res){res.sendFile(path.resolve(__dirname, "./views/register.html"))});
