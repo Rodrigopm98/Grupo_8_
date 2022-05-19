a8e63faa9ab5430668fdb1ed4f92b546d29926f4
Rodrigopm98 <rodrigo.rp885@gmail.com>
2022-05-19 18:50:06 -0300

    Hice el productDetail.html

--- app.js
+++ app.js
@@ -12,3 +12,4 @@ app.get("/", function(req,res){res.sendFile(path.resolve(__dirname, "./views/ind
 app.get("/productCart", function(req,res){res.sendFile(path.resolve(__dirname, "./views/productCart.html"))});
 app.get("/login", function(req,res){res.sendFile(path.resolve(__dirname, "./views/login.html"))});
 app.get("/register", function(req,res){res.sendFile(path.resolve(__dirname, "./views/register.html"))});
+app.get("/productDetail", function(req,res){res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))});
