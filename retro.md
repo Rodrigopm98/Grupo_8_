b1b6b148220f6e6c4db85862932034ea8741d68c
andresp90 <andresgpaulon@gmail.com>
2022-06-01 23:53:57 -0300

    archivos create y edit

--- retro.md
+++ retro.md
@@ -1,3 +1,4 @@
 En base a la devolución al primer sprint, "descomprimimos" la NavBar, pasando la información de contacto (redes) al footer.
 También agregamos la vista "Detalle de producto".
 Modificamos un poco el tablero de Trello para organizarnos. Lo cierto es que nuestra organización pasa más por Whatsapp por un tema de inmediatez.
+
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,5 +5,8 @@ const router = express.Router();
 const productController= require("../controller/productController");
 
 router.get("/productDetail", productController.detail);
-router.get("/productCart", productController.cart) 
+router.get("/productCart", productController.cart);
+router.get("/productCreate", productController.create);
+router.get("/productEdit", productController.edit);
+
 module.exports= router;
