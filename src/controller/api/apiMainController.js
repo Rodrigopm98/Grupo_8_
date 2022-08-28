61f4336740201ce10aa54af7ff5a13140bfc9443
andresp90 <andresgpaulon@gmail.com>
2022-08-28 16:59:50 -0300

    arreglado api users

--- src/controller/api/apiMainController.js
+++ src/controller/api/apiMainController.js
@@ -4,25 +4,26 @@ const apiMainController = {
     userList: (req, res) => {
         db.User.findAll()
         .then((usuarios) => {
+            let newData = usuarios.map(usuarios => {
+                return {
+                    id: usuarios.id,
+                    name: usuarios.firstName,
+                    last_name: usuarios.last_name,
+                    email: usuarios.email,
+                    endpoint: "api/users/" + usuarios.id
+                }
+            })         
             let respuesta = {
                 meta: {
                     status: 200,
                     count: usuarios.length,
                     url: "api/users"
                 },
-                data: {
-                    usuarios
-/*                     id: usuarios.id,
-                    name: usuarios.firstName + " " + usuarios.lastName,
-                    userName: usuarios.userName,
-                    email: usuarios.email,
-                    /* detail:  */ 
-            }}
+                data: newData
+            }
             res.json(respuesta);
         })
-    },
-
-    
+    },   
 };
 
 module.exports = apiMainController;
