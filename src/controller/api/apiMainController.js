2b5e2729188d05c2d703c68aa169b1010684c9d6
Rodrigopm98 <rodrigo.rp885@gmail.com>
2022-08-30 20:45:56 -0300

    api products

--- src/controller/api/apiMainController.js
+++ src/controller/api/apiMainController.js
@@ -24,35 +24,101 @@ const apiMainController = {
             res.json(respuesta);
         })
     },   
-    productList: (req, res) =>{
-        db.Product.findAll()
-        .then((productos) => {
-            let newProduct = productos.map(productos => {
-                return {
-                    id: productos.id,
-                    name: productos.name,
-                    description: productos.description,
-                    categoryId: productos.categoryId,
-                    detailUrl: "api/products/" + productos.id
+    productList:(req, res)=>{
+        db.Product.findAll( {include: [{ 
+            association: "categoria" 
+        },{
+            association: "talle"
+        },{ 
+            association: "marca"
+        },{
+            association: "deporte"
+        },{
+            association: "administrador"
+        }]})
+        .then((products)=>{
+         let productosDisponibles = products.filter(p => p.deleted == 0);
+         let productos= [];
+         for (let i = 0; i < productosDisponibles.length; i++) {
+            let producto = productosDisponibles[i].dataValues;
+            producto.url = "http://localhost:3030/api/products/"+ producto.id;
+            let dbBrand ;
+            if(producto.marca != null){
+                dbBrand = producto.marca.dataValues.brand;
+            }
+            let dbCategory = producto.categoria.dataValues.category;
+            let dbSize = producto.talle.dataValues.size;
+            let dbSport = producto.deporte.dataValues.sport;
+            let dbUser = producto.administrador.dataValues.firstName;
+
+            producto.dbRelations = [dbCategory, dbSize, dbSport]
+            if(dbBrand){
+                producto.dbRelations.push(dbBrand)
+            }
+            producto.dbRelations.push({admin:dbUser})
+    
+            delete producto.categoria;
+            delete producto.talle;
+            delete producto.marca;
+            delete producto.deporte;
+            delete producto.administrador;
+            productos.push(producto);
+         } 
+         return productos
+        })
+        .then((productos)=>{
+            let camisetas = 0;
+            let botines = 0;
+            let guantes = 0;
+            let otras = 0;
+            for (let i = 0; i < productos.length; i++) {
+                let productoCategoria = productos[i].dbRelations[0];
+                 switch (productoCategoria) {
+                    case "Camisetas":
+                        camisetas += 1
+                        break;
+                    case "Botines":
+                        botines += 1
+                        break;
+                    case "Guantes":
+                        guantes += 1
+                        break;
+                    case "Otras":
+                        otras += 1
+                        break;
+                    default:
+                        otras += 1
+                        break;
                 }
-            })         
+                
+            }
+         res.json({
+            count: productos.length,
+            countByCategory:{camisetas,botines,guantes,otras},
+            products: productos 
+        })
 
-            let respuesta = [{
-                meta:{
-                    status: 200,
-                    count: productos.length,
-                    url: "api/products"
-                },
-                /* countByCategory:{ */
-/* 
-                }, */
+        })
 
-                data: newProduct
-            }]
-            res.json(respuesta)
-        }) 
     },
+    detail:(req,res)=>{
+        db.Product.findOne({
+            where: {
+                id :req.params.id
+            },
+            include:[{association: "categoria"}]
+        })
+        .then((producto)=>{
+           /*  let product = producto.dataValues;
+            let ruta = path.join(__dirname, "../../public/images/") */
+           /*  product.url = "C:\Users\Peralta\OneDrive\Escritorio\DHC13\COPIA PROYECTO GLORIOSOS 29,8,22\Grupo_8_GloriososClub\public\images" + product.image; */
+            /* "http://localhost:3030/api/products/"+ product.id +"/"+ product.image; */
+            
+            res.json(producto)
 
+
+        })
+    }
  };
 
 module.exports = apiMainController;
--- src/routes/api/apiMainRouter.js
+++ src/routes/api/apiMainRouter.js
@@ -5,5 +5,6 @@ const apiMainController = require("../../controller/api/apiMainController.js");
 
 router.get("/users", apiMainController.userList);
 router.get("/products", apiMainController.productList);
+router.get("/products/:id", apiMainController.detail)
 
 module.exports = router;
