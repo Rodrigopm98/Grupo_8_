df111b673ff3a17c83d6180a7daeb77fdc2f86c2
Leandro Novoa <leandronovoac@gmail.com>
2022-09-01 22:42:31 -0300

    modificar config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -10,7 +10,7 @@ module.exports={
   "test": {
     "username": "root",
     "password": null,
-    /* "password": '1234', */
+   /*  "password": '1234', */
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
